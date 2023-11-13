79d82a9906f53a2393c2a9b254b2134ce8ba860c
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-13 04:40:00 -0700

    --Added more logic for user and atm

--- atm.cpp
+++ atm.cpp
@@ -17,10 +17,29 @@ int main() {
     // Check Balance, Deposit, Withdraw, Show Menu
 
     int option;
+    double balance = 500;
 
     showMenu();
     cout << "Option: ";
     cin >> option;
 
+    switch (option) {
+        case 1: cout << "Balance is: " << balance << " $" << endl; break;
+        case 2: cout << "Deposit amount:";
+            double depositAmount;
+            cin >> depositAmount;
+            balance += depositAmount;
+            break;
+        
+        case 3: cout << "Withdraw amount:";
+            double withdrawAmount;
+            cin >> withdrawAmount;
+            if (withdrawAmount <= balance)
+                balance -= withdrawAmount;
+            else 
+                cout << "Not enough money" << endl;
+            break;
+    }
+    
     system("pause>0");
 }
