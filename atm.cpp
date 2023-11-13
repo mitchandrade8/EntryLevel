a6d644c4a6fd36feb8096ff40913ac03fe0947a1
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-13 04:23:36 -0700

    first commit

---
+++ atm.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+
+void showMenu() {
+
+    cout << "**********MENU**********" << endl;
+    cout << "1. Check Balance" << endl;
+    cout << "2. Deposit" << endl;
+    cout << "3. Withdraw" << endl;
+    cout << "************************" << endl;
+
+}
+
+int main() {
+
+    // Check Balance, Deposit, Withdraw, Show Menu
+
+    showMenu();
+
+    system("pause>0");
+}
